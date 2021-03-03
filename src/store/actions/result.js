0bc474cb4084e934d8538957584c2b1b74f2c9b4
Jason Carrillo <jason.carrillo182@gmail.com>
2021-03-03 16:07:32 -0700

    moved results page to correct info

---
+++ src/store/actions/result.js
@@ -0,0 +1,22 @@
+import * as actionTypes from '../actions/actionsTypes'
+
+export const saveResult = (res) => {
+    return {
+        type: actionTypes.STORE_RESULT,
+        result: res
+    }
+}
+
+export const storeResult = (result) => {
+    return dispatch =>  {
+        setTimeout( () => {
+            dispatch(saveResult(result))
+        }, 2000);
+    }
+};
+export const deleteResult = (resultElId) => {
+    return {
+        type: actionTypes.DELETE_RESULT,
+        result: resultElId
+    }
+};
